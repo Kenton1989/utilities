af5275176c307a52ccc24a7980e797a0ac5ed00c
Kenton <38125586+Kenton1989@users.noreply.github.com>
2020-10-20 02:39:55 +0800

    add menu generation for this project
    Added:
    - util/generate_menu.py

---
+++ util/generate_menu.py
@@ -0,0 +1,43 @@
+"""
+Name: generate_menu
+========================
+Function:
+scan dir of this project and build a file system tree with MarkDown syntex.
+The resulting tree is printed to STDOUT.
+
+During building the tree:
+non-dir file in cwd is ignored.
+*/min/* is ignored.
+.exe files are ignored.
+hidden files (file name starts with ".") is ignored.
+"""
+
+import os
+import sys
+
+def ignore(fileName):
+    return (fileName.startswith('.') or
+            fileName == 'min' or
+            fileName.lower().endswith('.exe'))
+
+def generate_menu(fileName, parentDir, level = 0):
+    if ignore(fileName):
+        return
+
+    fullPath = os.path.join(parentDir, fileName)
+
+    print('  '*level + '- [' + fileName + '](' + fullPath + '): ')
+    
+    if os.path.isdir(fullPath):
+        for filePath in os.listdir(fullPath):
+            generate_menu(filePath, fullPath, level+1)
+
+
+if __name__ == "__main__":
+    for filePath in os.listdir(os.getcwd()):
+        if not os.path.isdir(filePath):
+            continue
+        generate_menu(filePath, '')
+
+    
+
